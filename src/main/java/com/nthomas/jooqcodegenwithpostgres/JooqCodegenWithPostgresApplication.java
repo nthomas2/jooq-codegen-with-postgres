ac104c634b0f099f3549c0b77fc012f316a437a8
Nathan Thomas <nate.p.thomas@gmail.com>
2019-04-11 10:07:48 -0400

    Initial commit

---
+++ src/main/java/com/nthomas/jooqcodegenwithpostgres/JooqCodegenWithPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.nthomas.jooqcodegenwithpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqCodegenWithPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqCodegenWithPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nthomas/jooqcodegenwithpostgres/JooqCodegenWithPostgresApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nthomas.jooqcodegenwithpostgres;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JooqCodegenWithPostgresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
